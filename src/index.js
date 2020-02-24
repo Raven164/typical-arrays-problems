06a835c07b30a37c11e339c0039f93276085fed3
Raven164 <i-s-pankratov@ya.ru>
2020-02-24 12:06:57 +0400

    <feat: add test>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array){
+	if ((array === undefined)||(array[0] === undefined)){return 0}
+	else{
+		let min = array[0];
+		for(i=0;i<array.length;i++){
+			let temp = array[i];
+			if(temp<min){
+			min=temp}
+			}
+		return min;
+	}
+}
+exports.max = function max (array){
+	if ((array == undefined)||(array == 0)){return 0}
+	else{
+		var max=array[0];
+		for(i=0;i<array.length;i++){
+			var temp=array[i];
+			if(temp>max){
+			max=temp}
+			}
+		return max;
+	}
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array){
+	if ((array == undefined)||(array == 0)){return 0}
+	else{
+		var count = 0;
+		for(i=0;i<array.length;i++){
+			count += array[i]
+			}
+		return count/array.length;
+	}
 }
